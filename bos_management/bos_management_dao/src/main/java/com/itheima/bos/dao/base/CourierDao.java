bde75e343bd5874a54c8b477b4ff7e2b3cd3c90f
onlyoveu <515494526@qq.com>
2017-12-22 15:02:17 +0800

    速运快递项目:调整文件文件夹位置

---
+++ bos_management/bos_management_dao/src/main/java/com/itheima/bos/dao/base/CourierDao.java
@@ -0,0 +1,13 @@
+package com.itheima.bos.dao.base;
+
+import com.itheima.bos.domain.base.Courier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CourierDao extends JpaRepository<Courier, Integer>, JpaSpecificationExecutor<Courier> {
+    @Query(value = "update Courier set deltag ='1' where id=?")
+    @Modifying
+    void deleteById(int id);
+}
